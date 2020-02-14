c26c5dbfbbd1364019ea92257e3ecf1498f79dc0
ClassroomAccount <jpepe44@gmail.com>
2020-02-14 11:25:55 -0500

    add comments

--- app/src/main/java/com/course/example/listintent/ListIntentActivity.java
+++ app/src/main/java/com/course/example/listintent/ListIntentActivity.java
@@ -21,12 +21,11 @@ public class ListIntentActivity extends Activity implements AdapterView.OnItemCl
 		setContentView(R.layout.main);
 
 		listview = (ListView)findViewById(R.id.list);
-		listview.setOnItemClickListener(this);
+		listview.setOnItemClickListener(this);    //connect listener
 
 		//notice custom format for list items
 		adapt = new ArrayAdapter<String> (this, R.layout.item, options);
 		listview.setAdapter(adapt);
-
 	}
 
 	//start activity based on list item selected
@@ -43,7 +42,7 @@ public class ListIntentActivity extends Activity implements AdapterView.OnItemCl
 			case 1 : {
 				Uri uri = Uri.parse("geo:0,0?q=175+forest+street+waltham+ma");
 				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-
+				//check to be sure Google Maps application is on device
 				if (intent.resolveActivity(getPackageManager()) != null) {
 					startActivity(intent);
 				}
@@ -51,7 +50,7 @@ public class ListIntentActivity extends Activity implements AdapterView.OnItemCl
 			}
 
 			case 2 : {
-
+				//notice no permission necessary because call not placed
 				Uri uri = Uri.parse("tel:8005551212");
 				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
 				startActivity(intent);
